9894330b3bc2feb1f4405fc0347de28398258902
dkurakin <dkur07@gmail.com>
2019-10-11 15:13:20 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,7 +1,3 @@
-# a = input()
-# print(f"It was: {a}")
-import time
-
 from twisted.internet import reactor
 from twisted.internet.protocol import ServerFactory, connectionDone
 from twisted.protocols.basic import LineOnlyReceiver
@@ -12,42 +8,45 @@ class Handler(LineOnlyReceiver):
     login: str
 
     def connectionLost(self, reason=connectionDone):
-        self.factory.user_list.remove(self)
-        print(f"Disconnected user: {self.login}")
+        self.factory.clients.remove(self)
+        print(f"{self.login} Disconnected")
 
     def connectionMade(self):
         self.login = None
-        self.factory.user_list.append(self)
-        print("New connection")
+        self.factory.clients.append(self)
+        print("New Connection")
+        self.sendLine("Type your NickName".encode())
 
-    def lineReceived(self, line):
+    def lineReceived(self, line: bytes):
         message = line.decode()
+
         if self.login is not None:
             message = f"<{self.login}>: {message}"
-            for user in self.factory.user_list:
-                if user is not self:
-                    user.sendLine(message.encode())
+
+            for user in self.factory.clients:
+                user.sendLine(message.encode())
         else:
-            if message.startswith("login:"):
-                login = message.replace("login:", "")
-                self.login = login
-                print(f"New user: {self.login}")
-                self.sendLine(f"Welcome, {self.login}!!".encode())
-            else:
-                self.sendLine("Unreadable login input".encode())
+            for user_names in self.factory.clients:
+                if user_names.login == message:
+                    self.sendLine(f"Логин {user_names.login} занят, попробуйте другой".encode())
+                    return
+            self.login = message
+            print(f"New user: {self.login}")
+            self.sendLine(f"Welcome, <{self.login}>!!!".encode())
 
 
 class Server(ServerFactory):
     protocol = Handler
-    user_list: list
+    clients: list
 
     def __init__(self):
-        self.user_list = []
+        self.clients = []
 
     def startFactory(self):
-        print("Server is running...")
+        print("Server started...")
 
 
-reactor.listenTCP(8443, Server())
-
+reactor.listenTCP(
+    7410, Server()
+)
 reactor.run()
